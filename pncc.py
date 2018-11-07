0be6bcbea163abd17f5c152fad4e2509bd9cd46a
Taiki Nakamura <37444351+supikiti@users.noreply.github.com>
2018-11-07 17:59:37 +0900

    Update pncc.py

--- pncc.py
+++ pncc.py
@@ -66,9 +66,9 @@ def weight_smoothing(final_output, medium_time_power, N=4, L=128):
             for l in range(final_output.shape[1]):
                 l_1 = max(l - N, 1)
                 l_2 = min(l + N, L)
-		spectral_weight_smoothing[m, l] = (1/float(l_2 - l_1 + 1)) * \
-                      sum([(final_output[m, l_] / medium_time_power[m, l_])
-                      for l_ in range(l_1, l_2)])
+                spectral_weight_smoothing[m, l] = (1/float(l_2 - l_1 + 1)) * \
+                    sum([(final_output[m, l_] / medium_time_power[m, l_])
+                    for l_ in range(l_1, l_2)])
 
     return spectral_weight_smoothing
 
